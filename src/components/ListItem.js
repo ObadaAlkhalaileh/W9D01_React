d034e75e9515742e9198516abf77c731a22109d6
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-24 20:00:22 +0400

    Pulse Check 11 modify

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,7 +4,7 @@ import React from 'react';
 // jsx
 const ListItem = ({todo,id}) => {
     const handleClick=(e)=>{
-        console.log(e.target.innertext);
+        console.log(e.target.innerText);
       }
     return < li onClick={handleClick} >{todo}</li>
 };
